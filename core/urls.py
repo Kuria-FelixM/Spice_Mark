43b6338bf8ffd2f23771200fd152a9b0e6a54c84
kuria_felix <muhiafelix35@gmail.com>
2024-09-06 20:17:36 +0300

    urls

--- core/urls.py
+++ core/urls.py
@@ -1,2 +1,30 @@
-$ urls
-#urls
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'vendors', VendorViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'blogs', BlogViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'payments', PaymentViewSet)
+router.register(r'coupons', CouponViewSet)
+router.register(r'taxes', TaxViewSet)
+router.register(r'notifications', NotificationViewSet)
+router.register(r'refunds', RefundViewSet)
+router.register(r'ordersitems', OrderItemViewSet)
+router.register(r'carts', CartViewSet)
+router.register(r'cartitems', CartItemViewSet)
+router.register(r'wishlists', WishlistViewSet)
+router.register(r'subscriptions', SubscriptionViewSet)
+router.register(r'contacts', ContactViewSet)
+router.register(r'analytics', AnalyticsViewSet)
+router.register(r'shippings', ShippingViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
